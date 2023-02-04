4ef53b53d01d50ce7f84de52f2d02c304e239b41
Pedro Uila <pedrouilac.sousa@gmail.com>
2023-02-03 23:21:10 -0300

    commit curso de python

--- Aprendendo Python/aula30-Variaveis_constantes_e_complexidades.py
+++ Aprendendo Python/aula30-Variaveis_constantes_e_complexidades.py
@@ -4,9 +4,21 @@ Muitas condições no mesmo if (ruim)
     <- Contagem de complexidade (ruim)
 """
 
-velocidade = 62 # 62Km
-local_carro = 90 # Km 90 da estrada
+velocidade = 61 # 62Km
+local_carro = 98 # Km 90 da estrada
 
-RADAR_1 = 60 # Velocidade máxima em Km
-LOCAL_1 = 100 # local do radar Km 100 da estrada
+RADAR01 = 60 # Velocidade máxima em Km
+LOCAL01 = 100 # local do radar Km 100 da estrada
 RADAR_RANGE = 1 # A distância que o radar pega
+
+velocidade_check = velocidade > RADAR01
+radar01_range = local_carro >= (LOCAL01 - RADAR_RANGE) and local_carro <= (LOCAL01 + RADAR_RANGE)
+multa_radar01 = radar01_range and velocidade_check
+
+if velocidade_check:
+    print("Velocidade do carro passou do radar 1")
+
+if multa_radar01:
+    print("O carro foi multado.")
+else:
+    print("Carro fora do radar.")
