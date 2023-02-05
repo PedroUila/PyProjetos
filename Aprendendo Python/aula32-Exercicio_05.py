85fec3681f98a5b0fd1866ba6e84bd7d668dcd7b
Pedro Uila <pedrouilac.sousa@gmail.com>
2023-02-05 20:28:52 -0300

    commit do curso de python

--- Aprendendo Python/aula32-Exercicio_05.py
+++ Aprendendo Python/aula32-Exercicio_05.py
@@ -1,13 +1,43 @@
-num = input("Digite um número: ")
+# num = input("Digite um número: ")
 
+# try:
+#     int_num = int(num)
+#     if int_num % 2 == 0:
+#         print("Esse número é PAR.")
+#     else:
+#         print("Esse número é IMPAR.")
+# except:
+#     print("Você não digitou um número...")
+
+
+
+# hora = input("Informe a hora atual: ")
+# try:
+#     hora = int(hora)
+#     dia = hora >= 5 and hora < 12
+#     tarde = hora >= 12 and hora < 18
+#     noite = hora >= 18 or hora < 5
+#     if dia:
+#         print("Bom dia!")
+#     elif tarde:
+#         print("Boa tarde!")
+#     elif noite:
+#         print("Boa noite!")
+# except:
+#     print("Digite apenas as horas.")
+
+
+
+nome = input("Digite seu primeiro nome: ")
 try:
-    int_num = int(num)
-    if int_num % 2 == 0:
-        print("Esse número é PAR.")
+    if len(nome) < 5:
+        print("Seu nome é curto.")
+    elif len(nome) > 6:
+        print("Seu nome é muito grande.")
     else:
-        print("Esse número é IMPAR.")
-
+        print("Seu nome é normal.")
 except:
-    print("Você não digitou um número...")
+    print("Digite seu nome.")
+
 
 
