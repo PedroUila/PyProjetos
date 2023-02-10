0a24f52793099f3aaf1c39997bd7c80cf29b1d3a
Pedro Uila <pedrouilac.sousa@gmail.com>
2023-02-10 09:08:07 -0300

    commit do curso de python

--- Aprendendo Python/aula40-Exercicio_07.py
+++ Aprendendo Python/aula40-Exercicio_07.py
@@ -1,41 +1,53 @@
 """ Calculadora com while """
 
-from time import sleep
-
 while True:
+    print("[+]soma [-]subtracao [x]multiplicação [/]divisão")
+    operacao = input("Digite a operação que deseja fazer: ").lower()
+    soma = operacao == "+"
+    subtracao = operacao == "-"
+    multiplicacao = operacao == "x"
+    divisao = operacao == "/"
+    operacao_valida = soma or subtracao or multiplicacao or divisao
+
+    if not(operacao_valida):
+        print("\nNão compreendi a operação.\n")
+        continue
+        
     while True:
-        print("[+]soma [-]subtracao [x]multiplicação [/]divisão")
-        operacao = input("Digite a operação que deseja fazer: ").lower()
-        soma = operacao == "+"
-        subtracao = operacao == "-"
-        multiplicacao = operacao == "x"
-        divisao = operacao == "/"
-
-        if soma or subtracao or multiplicacao or divisao:
+        try:
+            num1 = input("Digite um número: ")
+            num1 = int(num1)
+            num2 = input("Digite outro número: ")
+            num2 = int(num2)
             break
-        else:
-            print("\nNão compreendi.\n")
+        except:
+            print("Digite números apenas.")
 
-    num1 = input("Digite um número: ")
-    num2 = input("Digite outro número: ")
-    num1 = int(num1)
-    num2 = int(num2)
+    resultado = None
 
     if soma:
-        print(num1 + num2)
+        resultado = num1 + num2
     elif subtracao:
-        print(num1 - num2)
+        resultado = num1 - num2
     elif multiplicacao:
-        print(num1 * num2)
+        resultado = num1 * num2
     elif divisao:
-        print(num1 / num2)
+        resultado = num1 / num2
     
-    sair = input("Deseja encerrar o programa? [s]im [n]ão: ")
-    print("\n")
-    sair = sair.lower()
+    print(f"O resultado é {resultado}")
+
+
+    while True:
+        sair = input("Deseja encerrar o programa? [s]im [n]ão: ")
+        print("\n")
+        sair = sair.lower()
+        if sair == "s" or sair == "n":
+            break
+        else:
+            print("Digite s ou n...")
+    
     if sair == "s":
         break
-    elif sair != "s" and sair != "n":
-        print("Digite s ou n...")
-        continue
+
+
 print("Programa encerrado!")
