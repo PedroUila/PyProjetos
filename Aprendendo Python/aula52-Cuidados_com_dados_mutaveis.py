66dbe610db7e01b1ebb24b7702d87bdf3e8307ac
Pedro Uila <pedrouilac.sousa@gmail.com>
2023-02-18 23:21:00 -0300

    commit do curso de python

--- Aprendendo Python/aula52-Cuidados_com_dados_mutaveis.py
+++ Aprendendo Python/aula52-Cuidados_com_dados_mutaveis.py
@@ -4,7 +4,13 @@ Cuidados com dados mutáveis
 = - aponta para o mesmo valor na memória (mutável)
 """
 lista_a = ['Luiz', 'Maria', 1, True, 1.2]
-lista_b = lista_a.copy()
+# lista_b = lista_a faz uma referência direta à variável.
+# Portanto se a variável lista_a for alterada no futuro
+# a variável lista_b apareceria com a alteração feita
+
+lista_b = lista_a.copy() # É uma copia da lista_a
+# e mesmo se a lista_a fo alterada mais a frente do código
+# lista_b não será afetada
 
 lista_a[0] = 'Qualquer coisa'
 print(lista_a)
