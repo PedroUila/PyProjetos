04ceee644340a08fbca2ee04d5faaca02b388e5d
pedrouila2 <pedrouilac.sousa@gmail.com>
2023-04-23 22:14:39 -0300

    alterando Exercícios

---
+++ Aprendendo Python/Exercícios/Ex11.py
@@ -0,0 +1,17 @@
+"""
+Faça uma lista de comprar com listas
+O usuário deve ter a possibilidade de
+inserir, apagar e listar valores da sua lista
+Não permita que o programa quebre com 
+erros de índices inexistentes na lista.
+"""
+
+lista_compras = list()
+novo_item = ''
+interacao_lista = ''
+
+while True:
+
+    print('lista de compras')
+    novo_item = input('Deseja inserir: ')
+
